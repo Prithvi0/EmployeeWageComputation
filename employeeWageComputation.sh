2c6e90541935913d6d6993993d84af068d50786f
Prithvi0 <prithviy96@gmail.com>
2020-03-04 17:34:57 +0530

    Added daily employee wage program

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,17 +1,22 @@
 #!/bin/bash -x
 
+# CONSTANTS FOR THE PROGRAM
+WAGE_PER_HOUR=20
+FULL_DAY_HOUR=8
+
 # Check Employee is Present or Absent.
 echo "Welcome to Employee Wage Computation"
 
-# Function to check attendence
-function attendence_check () {
+# Function to check attendence and daily employee wage
+function dailyEmployeeWage () {
 	attendence=$((RANDOM%2))
 	if [[ $attendence -eq 1 ]]
 	then
 		echo "Employee is Present"
+		dailyWage=$(($FULL_DAY_HOUR*$WAGE_PER_HOUR))
 	else
 		echo "Employee is Absent"
+		dailyWage=0
 	fi
 }
-#calling function to get Employee's attendence status
-attendence_check
+dailyEmployeeWage
