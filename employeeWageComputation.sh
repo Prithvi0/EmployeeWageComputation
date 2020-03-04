1a364cc5db3f473777f6e1f35b471de71bd194e9
Prithvi0 <prithviy96@gmail.com>
2020-03-04 18:05:28 +0530

    full time and part time employee wages program using case statement

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -15,17 +15,19 @@ partTime=2
 # Function to check attendence, daily employee wage based on full time and part time
 function FullTimePartTimeEmployeeWage () {
 	attendence=$((RANDOM%3))
-	if [[ $attendence -eq $fullTime ]]
-	then
-		echo "Employee is Present"
-		dailyWage=$(($FULL_DAY_HOUR*$WAGE_PER_HOUR))
-	elif [[ $attendence -eq $partTime ]]
-	then
-		echo "Employee is Present"
-      dailyWage=$(($HALF_DAY_HOUR*$WAGE_PER_HOUR))
-	else
-		echo "Employee is Absent"
-		dailyWage=0
-	fi
+	case $attendence in
+		$fullTime )
+			echo "Employee is Present"
+			dailyWage=$(($FULL_DAY_HOUR*$WAGE_PER_HOUR))
+			;;
+		$partTime )
+			echo "Employee is Present"
+      	dailyWage=$(($HALF_DAY_HOUR*$WAGE_PER_HOUR))
+			;;
+		* )
+			echo "Employee is Absent"
+			dailyWage=0
+			;;
+	esac
 }
 FullTimePartTimeEmployeeWage
