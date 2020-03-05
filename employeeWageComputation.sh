2985111a0be603ba27ffc3c36269d134d97cd54a
Prithvi0 <prithviy96@gmail.com>
2020-03-05 12:10:58 +0530

    Added program to store the daily and total wage

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -34,8 +34,21 @@ function EmployeeWorkHours () {
 #Calculating Wages till Working days per month or Working hours per month is reached
 while [[ $totalWorkingHours -lt $WORKING_HOURS_PER_MONTH && $totalWorkingDays -lt $WORKING_DAYS_PER_MONTH ]]
 do
-	(( totalWorkingDays++ ))
+	((totalWorkingDays++))
 	workingHours="$(EmployeeWorkHours)"     # Calling the function
 	totalWorkingHours=$(($totalWorkingHours+$workingHours))
+
+	#calculating and storing the daily wage
+	dailyWage=$(($workingHours*$WAGE_PER_HOUR))
+	dailyWage[$totalWorkingDays]=$dailyWage
+
+	#calculating and storing the total wages
+	totalWage=$(($totalWage+$dailyWage))
+	totalWage[totalWorkingDays]=$totalWage
 done
-totalWage=$(($totalWorkingHours*$WAGE_PER_HOUR))
+
+for ((i=1; i<${#dailyWage[@]}; i++))
+do
+	echo "Day[$i]:${dailyWage[$i]}"
+done
+echo "TotalWage:$totalWage"
