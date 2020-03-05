af4b5c085b939500e8650ae5cd8e7e01b3d0782b
Prithvi0 <prithviy96@gmail.com>
2020-03-05 09:51:17 +0530

    Added program for function to get work hours

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -5,42 +5,37 @@ echo "Welcome to Employee Wage Computation"
 
 # CONSTANTS FOR THE PROGRAM
 WAGE_PER_HOUR=20
-FULL_DAY_HOUR=8
-HALF_DAY_HOUR=4
 WORKING_DAYS_PER_MONTH=20
 WORKING_HOURS_PER_MONTH=100
 
 #VARIABLES
 fullTime=1
 partTime=2
-monthlyWage=0
 daysWorked=0
 hoursWorked=0
 
 # Function to check attendence, daily employee wage based on full time and part time
-function FullTimePartTimeEmployeeWage () {
-	attendence=$((RANDOM%3))
-	case $attendence in
+function EmployeeWorkHours () {
+	employeeCheck=$((RANDOM%3))
+	case $employeeCheck in
 		$fullTime )
-			echo "Employee is Present"
-			empHours=8
+			workingHours=8
 			;;
 		$partTime )
-			echo "Employee is Present"
-         empHours=4
+			workingHours=4
 			;;
 		* )
-			echo "Employee is Absent"
-         empHours=0
+			workingHours=0
 			;;
 	esac
-	hoursWorked=$(($hoursWorked+$empHours))
+	echo "$workingHours"
 }
 
 #Calculating Wages till Working days per month or Working hours per month is reached
 while [[ $daysWorked -lt $WORKING_DAYS_PER_MONTH && $hoursWorked -lt $WORKING_HOURS_PER_MONTH ]]
 do
-   (( daysWorked++ ))
-	FullTimePartTimeEmployeeWage     # Calling the function
+	(( daysWorked++ ))
+	workingHours="$(EmployeeWorkHours)"     # Calling the function
+	totalWorkingHours=$(($totalWorkingHours+$workingHours))
 done
-totalWage=$(($hoursWorked*$WAGE_PER_HOUR))
+totalWage=$(($totalWorkingHours*$WAGE_PER_HOUR))
