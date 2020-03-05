3821ee46b710354a85bce12899e2747a2415c191
Prithvi0 <prithviy96@gmail.com>
2020-03-05 12:47:59 +0530

    Added program to store the day, the daily and the total wage

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -8,13 +8,13 @@ WAGE_PER_HOUR=20
 WORKING_DAYS_PER_MONTH=20
 WORKING_HOURS_PER_MONTH=100
 
-#VARIABLES
+# VARIABLES
 fullTime=1
 partTime=2
 totalWorkingDays=0
 totalWorkingHours=0
 
-# Function to check attendence, daily employee wage based on full time and part time
+# Function to check Attendence, daily Employee Wage based on Full time and Part time
 function EmployeeWorkHours () {
 	employeeCheck=$((RANDOM%3))
 	case $employeeCheck in
@@ -31,24 +31,25 @@ function EmployeeWorkHours () {
 	echo "$workingHours"
 }
 
-#Calculating Wages till Working days per month or Working hours per month is reached
+# Calculating Wages till Working Days per month or Working Hours per month is reached
 while [[ $totalWorkingHours -lt $WORKING_HOURS_PER_MONTH && $totalWorkingDays -lt $WORKING_DAYS_PER_MONTH ]]
 do
 	((totalWorkingDays++))
 	workingHours="$(EmployeeWorkHours)"     # Calling the function
 	totalWorkingHours=$(($totalWorkingHours+$workingHours))
 
-	#calculating and storing the daily wage
+	# Calculating and storing the Daily Wage
 	dailyWage=$(($workingHours*$WAGE_PER_HOUR))
 	dailyWage[$totalWorkingDays]=$dailyWage
 
-	#calculating and storing the total wages
+	# Calculating and storing the Total Wages
 	totalWage=$(($totalWage+$dailyWage))
 	totalWage[totalWorkingDays]=$totalWage
 done
 
-for ((i=1; i<${#dailyWage[@]}; i++))
+# Storing the Day with Daily Wages
+for i in ${!dailyWage[@]}
 do
-	echo "Day[$i]:${dailyWage[$i]}"
+	echo "Day $i:${dailyWage[$i]}"
 done
 echo "TotalWage:$totalWage"
